285a587639a487ee9d37a7bd0e1cd6a0677ff3da
makzimillian <maxmcgowan@gmail.com>
2014-10-22 14:51:29 +0800

    installed carrierwave gem

--- spec/features/upload_image_spec.rb
+++ spec/features/upload_image_spec.rb
@@ -4,6 +4,11 @@ feature 'Visit the image uploader page' do
   scenario 'Upload Image!' do
     visit '/'
     vist route_path
-    expect(page).to have_content 'Image'
+    expect(page).to have_content 'Image Uploader'
   end
+
+
+  scenario 'Upload Succesful'
+
+  scenario 'Show the image'
 end
--- spec/features/visit_home_spec.rb
+++ spec/features/visit_home_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
-feature 'Visit the home page' do |variable|
+feature 'Visit the home page' do
   scenario 'Hello World!' do
     visit '/'
     expect(page).to have_content 'Hello World!'
   end
 
 
-scenario 'Privacy' do
-  visit root_path
-  click_link 'Privacy'
-  expect(page).to have_content 'Privacy'
-
+  scenario 'Privacy' do
+    visit root_path
+    click_link 'Privacy'
+    expect(page).to have_content 'Privacy'
+  end
 
 
 end
